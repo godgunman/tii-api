b21402dd9137c240f68e76295de09cac6078ad59
ggm <ggm@skymizer.com>
2015-11-08 09:45:45 +0200

    implement parse api

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 var express = require('express');
 var app = express();
+var Parse = require('node-parse-api').Parse;
+
+
+var APP_ID = 'y7K3CapPOCqhs5zz3cafXIOX2d30yrV6bUMfsFZo';
+var MASTER_KEY = 'lfjaejocphcwrjuWIYjYSejH5lAOsPnjA5Yx0Orc';
+var parseApp = new Parse(APP_ID, MASTER_KEY);
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -10,11 +16,17 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+    response.render('pages/index');
+});
+
+app.get('/order', function(request, response) {
+    parseApp.find('Order', '', function (err, queryResponse) {
+        response.json(queryResponse);
+    });
 });
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+    console.log('Node app is running on port', app.get('port'));
 });
 
 
